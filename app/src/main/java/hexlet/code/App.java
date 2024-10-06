d8bba17cc3fec49b8b28339f7829f5818b5ba46e
AntonRaevsky <aanraevskiy@sberbank.ru>
2024-10-06 14:47:47 +0300

    add files support in cli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ package hexlet.code;
 public class App {
 
     public static void main(String[] args) {
+        System.out.println();
         Cli.main(args);
     }
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,11 +2,29 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
+
+import java.io.File;
 import java.util.concurrent.Callable;
 
-@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "Differ 1.0",
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 class Cli implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "path to first file")
+    private File firstFile;
+
+    @Parameters(index = "0", description = "path to second file")
+    private File secondFile;
+
+    @Option(
+            names = {"-f", "--format"},
+            description = "output format",
+            showDefaultValue = CommandLine.Help.Visibility.ALWAYS,
+            defaultValue = "stylish"
+    )
+    private String format = "format";
 
     @Override
     public Integer call() {
