5d06bfe1cb40ac82fedcf4dffeaf26f542611712
AntonRaevsky <aanraevskiy@sberbank.ru>
2024-10-16 22:24:59 +0300

    normalize path

--- src/test/java/hexlet/code/Utils.java
+++ src/test/java/hexlet/code/Utils.java
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 
 public class Utils {
     public static String readFromFileLocation(String fileLocation) throws IOException {
-        Path resultPath = Path.of(fileLocation);
+        Path resultPath = Path.of(fileLocation).toAbsolutePath().normalize();
         byte[] fileBytes = Files.readAllBytes(resultPath);
         return new String(fileBytes, StandardCharsets.UTF_8);
     }
