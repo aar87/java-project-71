25c0862ce6ec20aac26f9dbc73726559e2a9549c
AntonRaevsky <aanraevskiy@sberbank.ru>
2024-10-16 22:31:15 +0300

    use paths

--- src/test/java/hexlet/code/Utils.java
+++ src/test/java/hexlet/code/Utils.java
@@ -4,10 +4,11 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Utils {
     public static String readFromFileLocation(String location) throws IOException {
-        Path resultPath = Path.of(System.getProperty("user.dir") + "/" + location).toAbsolutePath().normalize();
+        Path resultPath = Paths.get(System.getProperty("user.dir") + "/" + location).toAbsolutePath().normalize();
         byte[] fileBytes = Files.readAllBytes(resultPath);
         return new String(fileBytes, StandardCharsets.UTF_8);
     }
